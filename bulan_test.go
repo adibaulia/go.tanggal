126d6e8228f00282c135d53486551dc410e40e86
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-10-09 17:14:25 +0700

    test: Create Unit Test For Package `bulan`
    if applied, this commit will create unit test for package `bulan`

---
+++ bulan_test.go
@@ -0,0 +1,50 @@
+package bulan_test
+
+import (
+	"testing"
+	"time"
+
+	tanggal "github.com/adibaulia/go.tanggal"
+	"github.com/stretchr/testify/assert"
+)
+
+const ISO_LAYOUT string = "2006-01-02"
+const DATE_IN_STRING string = "2000-12-15"
+
+var date time.Time
+
+func init() {
+	dateInDate, _ := time.Parse(ISO_LAYOUT, DATE_IN_STRING)
+	date = dateInDate
+}
+
+func Test_InitNewBulan(t *testing.T) {
+	bulan := tanggal.NewBulan(date)
+	assert.Equal(t, "", string(bulan))
+}
+
+func Test_NewBulanToString(t *testing.T) {
+	var bulan interface{} = tanggal.NewBulan(date).ToString()
+	_, ok := bulan.(string)
+	assert.Equal(t, true, ok)
+}
+
+func Test_NewBulanLong(t *testing.T) {
+	bulan := tanggal.NewBulan(date).Long().ToString()
+	assert.Equal(t, "Desember", bulan)
+}
+
+func Test_NewBulanShort(t *testing.T) {
+	bulan := tanggal.NewBulan(date).Short().ToString()
+	assert.Equal(t, "Des", bulan)
+}
+
+func Test_NewBulanUpperCase(t *testing.T) {
+	bulan := tanggal.NewBulan(date).UpperCase().ToString()
+	assert.Equal(t, "DESEMBER", bulan)
+}
+
+func Test_NewBulanLowerCase(t *testing.T) {
+	bulan := tanggal.NewBulan(date).LowerCase().ToString()
+	assert.Equal(t, "desember", bulan)
+}
